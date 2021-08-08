a70a29c4c43369d5e9d8d6c16a59d9194dd07bac
KYJ <kimyeji2902@gmail.com>
2021-08-08 23:27:17 +0900

    Component-jsx Practice

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import './App.css';
 import ChildComponent from './child.js';
 
 function App() {
+  const text = "인프런 수강생 여러분 화이팅!";
+  const sayHello = function() {
+    return <h3>인프런 강의 너무 좋아</h3>;
+  }
+  const sayHello2 = function() {
+    alert('안녕하세요');
+  }
   return (
     <div>
       <h1>안녕하세요!</h1>
+      <h2>{text}</h2>
+      {sayHello()}
+      <div onClick = {function() {
+        alert('안녕');
+      }}>클릭해주세요</div>
       <ChildComponent />
       <ChildComponent />
       <ChildComponent />
